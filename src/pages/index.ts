87a9316e712724552ae4cc797520f05ce0321c5c
Кристина Мищенко <99183284+Doc-Kristen@users.noreply.github.com>
2023-10-18 16:17:15 +0300

    service: создает json-server

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -3,4 +3,3 @@ export { default as Doctor } from './Doctor'
 export { default as Authorization } from './Authorization'
 export { default as NotFound } from './NotFound'
 export { default as Main } from './Main'
-
