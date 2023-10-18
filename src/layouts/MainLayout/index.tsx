d8fcc31ca0eee044ac557a87a8c9f8c26757b727
Кристина Мищенко <99183284+Doc-Kristen@users.noreply.github.com>
2023-10-18 10:22:40 +0300

    feat: создает Header

--- src/layouts/MainLayout/index.tsx
+++ src/layouts/MainLayout/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import { Container } from '@mui/material'
+import { Header } from '@components/index'
 
 const MainLayout: React.FC = () => {
 	return (
 		<>
+			<Header />
 			<Container style={{ paddingTop: 80 }}>
 				<Outlet />
 			</Container>
