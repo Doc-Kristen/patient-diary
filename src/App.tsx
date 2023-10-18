f48a57493bc91ffc44f58f929981308a079a6c47
Кристина Мищенко <99183284+Doc-Kristen@users.noreply.github.com>
2023-10-18 10:02:46 +0300

    service: создает сборку

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+const App: React.FC = () => {
+	return <h1>Стартовая сборка</h1>
+}
+
+export default App
---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [react()],
+	resolve: {
+		alias: {
+			'@components': '/src/components',
+			'@layouts': '/src/layouts',
+			'@styles': '/src/styles',
+			'@types': '/src/types',
+			'@pages': '/src/pages',
+			'@assets': '/src/assets',
+			'@hooks': '/src/hooks',
+			'@images': '/src/assets/images',
+			'@utils': '/src/utils',
+			'@store': '/src/store',
+			'@services': '/src/services',
+		},
+	},
+})
